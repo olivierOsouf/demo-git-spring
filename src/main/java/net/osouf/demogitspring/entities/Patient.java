c867b97920cf7e46bf2245661d9bb040d27c5461
olivier osouf <olivierosouf@gmail.com>
2025-12-02 22:14:55 +0100

    commit à t=45_22 de la video

--- src/main/java/net/osouf/demogitspring/entities/Patient.java
+++ src/main/java/net/osouf/demogitspring/entities/Patient.java
@@ -1,14 +1,12 @@
 package net.osouf.demogitspring.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 @Entity
 @NoArgsConstructor @AllArgsConstructor @Getter @Setter @ToString @Builder
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length=30, nullable = false)
     private String nom;
 }
--- src/main/java/net/osouf/demogitspring/web/PatientController.java
+++ src/main/java/net/osouf/demogitspring/web/PatientController.java
@@ -3,9 +3,12 @@ package net.osouf.demogitspring.web;
 import lombok.AllArgsConstructor;
 import net.osouf.demogitspring.entities.Patient;
 import net.osouf.demogitspring.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,9 +18,12 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> patientList = patientRepository.findAll();
-        Model patientsList = model.addAttribute("listPatients", patientList);
+    public String index(Model model, @RequestParam(name="page", defaultValue = "0") int page,
+                            @RequestParam(name = "size", defaultValue = "3") int size) {
+        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page, size));
+        model.addAttribute("listPatients", pagePatients.getContent());
+        model.addAttribute("size",size);
+        model.addAttribute("pages", new int [pagePatients.getTotalPages()]);
         return "patients";
     }
 }
